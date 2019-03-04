023e81b195863786858ff9647f698bc26e6fc31e
nagabhushanamn <nagabhushanamn@gmail.com>
2019-03-04 09:38:22 +0530

    commit

--- node-app/routes/index.js
+++ node-app/routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node-app' });
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'welcome to Node-app' });
 });
 
 module.exports = router;
