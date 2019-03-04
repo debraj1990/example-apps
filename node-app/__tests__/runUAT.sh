2d7428c8d404243099400ab035bf6e779e677bf2
nagabhushanamn <nagabhushanamn@gmail.com>
2019-03-04 10:40:36 +0530

    fix

--- node-app/__tests__/runUAT.sh
+++ node-app/__tests__/runUAT.sh
@@ -2,7 +2,7 @@
 #!/bin/bash
 
 # set variables
-hostname='localhost'
+hostname='18.222.122.191'
 port=$1
 
 # wait for the app to start
@@ -12,7 +12,7 @@ sleep 5
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
 
 expected_code='200'
-echo "${status_code}"
+
 if [ $status_code -eq $expected_code ];
 then
 	echo "PASS: ${hostname}:${port} is reachable"
