ab49e59f9ca1a76f2239cf13c4cabda1328180fe
nagabhushanamn <nagabhushanamn@gmail.com>
2019-03-04 10:27:04 +0530

    UAT

--- node-app/__tests__/runUAT.sh
+++ node-app/__tests__/runUAT.sh
@@ -11,12 +11,12 @@ sleep 5
 # ping the app
 status_code=$(curl --write-out %{http_code} --out /dev/null --silent ${hostname}:${port})
 
-echo "${status_code}"
+expected_code=200
 
-# if [ $status_code == 200 ];
-# then
-# 	echo "PASS: ${hostname}:${port} is reachable"
-# else
-# 	echo "FAIL: ${hostname}:${port} is unreachable"
-#     exit 1
-# fi
+if [ $status_code == $expected_code ];
+then
+	echo "PASS: ${hostname}:${port} is reachable"
+else
+	echo "FAIL: ${hostname}:${port} is unreachable"
+    exit 1
+fi
