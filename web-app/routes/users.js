0e33078304c330d79758596594f089400476e04f
Nag-Training <nag-training@nag.e7f-in.ibmmobiledemo.com>
2019-03-06 10:39:20 +0530

    change

--- web-app/routes/users.js
+++ web-app/routes/users.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  res.send('hello user');
+  res.send('hi user');
 });
 
 module.exports = router;
