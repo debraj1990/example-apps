d25990463d86bb383c271e0f2fd5da00281d8b4f
Nag-Training <nag-training@nag.e7f-in.ibmmobiledemo.com>
2019-03-06 10:13:55 +0530

    initial commit

---
+++ products-catalog-v1/model/User.js
@@ -0,0 +1,19 @@
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+    userName: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
