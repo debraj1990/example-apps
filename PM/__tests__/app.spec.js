4287fe60c441da2b1d09122a2b51d2d2f75e0f72
Nag-Training <nag-training@NAGABHUSHANAMs-MacBook-Pro.local>
2019-03-07 23:21:43 +0530

    test

--- PM/__tests__/app.spec.js
+++ PM/__tests__/app.spec.js
@@ -6,7 +6,7 @@ var mongoose=require('mongoose')
 
 describe('GET /', function () {
     it('should return title', function (done) {
-        request(app).get('/').expect('products-catalog-api', done);
+        request(app).get('/').expect('PM', done);
     });
     it('should be true',()=>{
         expect(true).toBe(true)
